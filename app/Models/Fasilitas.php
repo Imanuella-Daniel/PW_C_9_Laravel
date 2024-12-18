0b0ad68e50a1121cc4db711d54f9ad8e03cb6e65
Imanuella Daniel <imanuelladaniel@gmail.com>
2024-12-18 08:21:46 +0700

    readme

--- app/Models/Fasilitas.php
+++ app/Models/Fasilitas.php
@@ -8,17 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Fasilitas extends Model
 {
     use HasFactory;
-
-    // Define the table associated with the model
     protected $table = 'fasilitas';
-
-    // Define the primary key
     protected $primaryKey = 'IDFasilitas';
 
-    // Set to false if the primary key is not auto-incrementing
     public $incrementing = true;
 
-    // Define the fields that are mass assignable
     protected $fillable = [
         'IDFasilitas',
         'NamaFasilitas',
